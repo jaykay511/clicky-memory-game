4c9f8dbb6b4bc62b7ce9e60b303ea9e93ec43d39
Jenn <jenn@Jenns-MacBook-Pro.local>
2019-06-27 16:19:58 -0700

    Updates

---
+++ precache-manifest.4a6aad6b71afe1d0c9679b62f19d32ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdf816a352e9e7c54a6fcfcba42c836",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "03e9e8586d44b1d84bce",
+    "url": "/clicky-memory-game/static/css/main.df0cd463.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/clicky-memory-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "03e9e8586d44b1d84bce",
+    "url": "/clicky-memory-game/static/js/main.cb71366a.chunk.js"
+  },
+  {
+    "revision": "2d83433aa8872a544556",
+    "url": "/clicky-memory-game/static/js/runtime~main.83671481.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-// root is in index.html
-ReactDOM.render(<App />, document.getElementById('root'));
